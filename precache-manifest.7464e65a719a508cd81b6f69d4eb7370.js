10c3f2ec7a07be032c2a51e38678c195fec46989
millie33 <Milly339@protonmail.com>
2020-01-13 21:54:21 -0500

    Updates

---
+++ precache-manifest.7464e65a719a508cd81b6f69d4eb7370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d08f3f29e81ea41cb9ae2777977c00f",
+    "url": "/Random_Quote_Machine/index.html"
+  },
+  {
+    "revision": "8546133d8702739589cc",
+    "url": "/Random_Quote_Machine/static/css/main.5bebd04a.chunk.css"
+  },
+  {
+    "revision": "ecfbc76ed68608b23c49",
+    "url": "/Random_Quote_Machine/static/js/2.2db7b3f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Random_Quote_Machine/static/js/2.2db7b3f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8546133d8702739589cc",
+    "url": "/Random_Quote_Machine/static/js/main.95ac0c2d.chunk.js"
+  },
+  {
+    "revision": "cdd811d8c377a41d3db3",
+    "url": "/Random_Quote_Machine/static/js/runtime-main.5de3552d.js"
+  }
+]);
